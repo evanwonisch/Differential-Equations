0b5fea7973606c92c9bce9c8de4e881afc8828a9
evanwonisch <evanwonisch@users.noreply.github.com>
2020-10-16 23:12:05 +0200

    Update Graphics and Equations

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ var width, height;
 var time = 0;
 const FPS = 60;
 
-var xs = [-15,15, 1]
-var ys = [-10,10, 1]
+//Coordinate System Size and units
+var xs = [-15,15, 0.9]
+var ys = [-10,10, 0.9]
 
 var plot_width;
 var plot_heigth;
@@ -17,6 +18,7 @@ var yshift;
 var mouseX;
 var mouseY;
 
+//Menu variables
 var showFlow = false;
 var equation_index = 0;
 var stepsize = 0.1;
@@ -25,7 +27,7 @@ window.onload = () => {
     canvas = document.getElementById("canvas")
     ctx = canvas.getContext("2d")
 
-
+    //Update Loop
     setInterval(() => {
         canvas.width = width = window.innerWidth
         canvas.height = height = window.innerHeight
@@ -36,11 +38,13 @@ window.onload = () => {
 
     }, 1000 / FPS);
 
+    //Mouse Position
     window.addEventListener("mousemove", e => {
         mouseX = e.pageX;
         mouseY = e.pageY;
     })
 
+    //Equation selector
     var equations = document.querySelectorAll("li")
     equations.forEach(e => {
         e.addEventListener("click", e => {
@@ -52,12 +56,14 @@ window.onload = () => {
         })
     })
 
+    //Streamlines selector
     var chekbox = document.getElementById("checkbox")
     chekbox.addEventListener("change", e => {
         showFlow = e.target.checked
     })
 
 
+    //Eulers Method Stepsize Parameter
     var numberInput = document.getElementById("numberInput")
     numberInput.addEventListener("keydown", e => {
         if(e.code == "Enter"){
@@ -70,6 +76,18 @@ window.onload = () => {
     })
 }
 
+
+
+
+
+
+
+/**
+ * Main Differential Equation
+ * @param {Number} x
+ * @param {Number} y
+ * @returns {Number} Derivative of the objective function f(x) at each point in the field
+ */
 function f_prime(x, y){
     switch(equation_index){
         case 0:
@@ -77,17 +95,17 @@ function f_prime(x, y){
         case 1:
             return -y * Math.exp(-0.05*(x*x+y*y))
         case 2:
-            return 0.1*(x*Math.sin(y))
+            return x/(y*y+1)
         case 3:
             return (x*x-y*y)/(x*x+y*y)
         case 4:
-            return Math.sin(x) * Math.cos(y)
+            return Math.sin(x*y * 0.5) + Math.cos(x)
         case 5:
             return 0.1*((x*x -2)/(y*y+1)*Math.sin(x)+ Math.sin(y))
     }
 }
 
-
+//Main Loop
 function update(){
 
     plot_width = xs[1] - xs[0]
@@ -99,28 +117,35 @@ function update(){
 
     time += 0.3
 
+    //Plotting Direction Field
     for(var x = xs[0]; x < xs[1]; x+=xs[2]){
         for(var y = ys[0]; y < ys[1]; y+=ys[2]){
             if(showFlow){
+
+                //Drawing Streamlines
                 solve(x,y, "blue", 0.2, false)
             } else {
+
+                //Drawing Direction Field
+
+                var dx = 20 *  1 / Math.sqrt(1 + Math.pow(f_prime(x,y), 2))
+                var dy = dx * f_prime(x,y)
+
+
                 ctx.beginPath()
                 ctx.moveTo(x * xscale + xshift, -y * yscale + yshift)
-                ctx.lineTo(x * xscale + xshift + 10, -y * yscale + yshift - f_prime(x,y)* 10)
+                ctx.lineTo(x * xscale + xshift + dx, -y * yscale + yshift - dy)
                 ctx.stroke()
-
-                // ctx.font = "12px Arial";
-                // ctx.strokeStyle = "black"
-                // ctx.strokeText(x + " "+ y + " " , x*xscale+xshift, -y*yscale+yshift);
             }
         }
     }
 
+    //Solving at Mouse Position
     solve(mouseX / xscale - xshift/ xscale, -mouseY/yscale + yshift/yscale)
 }
 
 function solve(x, y, color = "red", lineWidth = 3, showPoint = true){
-    var n_steps = 40/ stepsize
+    var n_steps = 40 / stepsize
     var startx = x
     var starty = y;
     
@@ -138,6 +163,7 @@ function solve(x, y, color = "red", lineWidth = 3, showPoint = true){
 
     var step = 0;
     do {
+        //Draw Line
         if(step == 0){
             ctx.moveTo(x * xscale + xshift, -y * yscale + yshift)
         } else {
@@ -169,6 +195,7 @@ function solve(x, y, color = "red", lineWidth = 3, showPoint = true){
 
     var step = 0;
     do {
+        //Draw Line
         if(step == 0){
             ctx.moveTo(x * xscale + xshift, -y * yscale + yshift)
         } else {
